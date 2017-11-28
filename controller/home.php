fc1abb614adbc5069db8eeacff0eedaced306520
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-11-28 16:58:12 +0100

    added constructor to managers

---
+++ controller/home.php
@@ -0,0 +1,3 @@
+<?php 
+require_once '../model/data.php';
+require_once '../services/services.php';
---
+++ model/BookManager.php
@@ -0,0 +1,18 @@
+<?php
+
+class BookManager {
+    private $_db;
+    
+    // constructor just calls connection to database
+    public function __construct($db)
+    {
+        $this->setDb($db);
+    }
+    
+     //SETTER
+    private function setDb(PDO $db)
+    {
+        $this->_db = $db;
+    }
+     // METHODS
+}
